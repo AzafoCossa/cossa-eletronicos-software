7fa0ad7ac33849eb9b0aefd093b532d5fc3c70fb
Azafo Cossa <azafoalexandre@outlook.com>
2026-04-17 21:47:39 +0200

    Enhance login functionality with validation and Livewire integration for email/phone and password

--- resources/views/components/front/auth/⚡login/login.blade.php
+++ resources/views/components/front/auth/⚡login/login.blade.php
@@ -9,30 +9,30 @@
           <h1 class="text-center text-6xl text-dark font-black">Seja bem-vindo</h1>
           <p class="text-center text-2xl mt-2">Registe-se e comece as tuas compras, hoje.</p>
 
-          <form action="#" class="w-full flex flex-col mt-20 text-2xl text-dark/70">
-
+          <form wire:submit.prevent="login" class="w-full flex flex-col mt-20 text-2xl text-dark/70">
+            <div class="text-center text-danger text-lg">@error('invalid_credentials') {{ $message }} @enderror</div>
             <div class="flex flex-col gap-2.5">
               <label for="full_name">Email / Celular</label>
-              <input type="text" id="email_or_phone" placeholder="Digite o seu ou numero de celular" class="bg-gray-200 rounded-xl py-4 px-6">
+              <input wire:model="email_or_phone" type="text" id="email_or_phone" placeholder="Digite o seu ou numero de celular" class="bg-gray-200 rounded-xl py-4 px-6 @error('email_or_phone') border-2 border-red-500 @enderror @error('invalid_credentials') border-2 border-red-500 @enderror">
             </div>
 
             <div class="flex flex-col gap-2.5 mt-10">
               <label for="full_name">Password</label>
-              <input type="text" id="password" placeholder="Digite uma password" class="bg-gray-200 rounded-xl py-4 px-6">
+              <input wire:model="password" type="password" id="password" placeholder="Digite uma password" class="bg-gray-200 rounded-xl py-4 px-6 @error('password') border-2 border-red-500 @enderror @error('invalid_credentials') border-2 border-red-500 @enderror">
             </div>
 
             <div class="flex justify-between mt-5">
               <div class="flex gap-2.5 items-center">
-                <input type="checkbox" id="remember_me" class="bg-gray-300 rounded-xl py-4 px-6 h-6 w-6">
+                <input wire:model="remember_me" type="checkbox" id="remember_me" class="bg-gray-300 rounded-xl py-4 px-6 h-6 w-6">
                 <label for="remember_me">Manter sessao iniciada</label>
               </div>
 
               <a href="forgot-password.html" class="text-primary">Esqueceu a senha?</a>
             </div>
 
-            <button class="bg-primary rounded-xl py-4 text-white mt-10 cursor-pointer">Registar</button>
+            <button type="submit" class="bg-primary rounded-xl py-4 text-white mt-10 cursor-pointer">Registar</button>
 
-            <p class="text-center text-gray-500 mt-10">Não tem uma conta? <a href="register.html" class="text-primary">Registe-se aqui.</a></p>
+            <p class="text-center text-gray-500 mt-10">Não tem uma conta? <a href="{{ route('register') }}" class="text-primary">Registe-se aqui.</a></p>
           </form>
         </div>
       </div>
--- resources/views/components/front/auth/⚡login/login.php
+++ resources/views/components/front/auth/⚡login/login.php
@@ -1,8 +1,50 @@
 <?php
 
+use App\Models\UserIdentifier;
+use App\Rules\EmailOrPhone;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public string $email_or_phone;
+    public string $password;
+    public bool $remember_me = false;
+
+    public function login(){
+        $this->validate([
+            'email_or_phone' => ['required', new EmailOrPhone()],
+            'password' => 'required',
+        ]);
+
+        $value = $this->email_or_phone;
+
+        if(isPhone($value)){
+            $value = addPhonePrefix($value);
+        }
+
+        $userIdentifier = UserIdentifier::where('value', $value)->with('user')->first();
+
+        if(!$userIdentifier){
+            throw ValidationException::withMessages(
+                ['invalid_credentials' => 'As credenciais fornecidas sao invalidas!'],
+            );
+        }
+
+        $user = $userIdentifier->user;
+
+        if(!Hash::check($this->password, $user->password)){
+            throw ValidationException::withMessages(
+                ['invalid_credentials' => 'As credenciais fornecidas sao invalidas, verifique e tente novamento!'],
+            );
+        }
+
+        Auth::login($user, $this->remember_me);
+
+        session()->regenerate();
+
+        return redirect()->intended('home');
+    }
 };
