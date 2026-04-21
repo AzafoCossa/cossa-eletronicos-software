850754a61532e1673b2df2f48e215312f69ae437
Azafo Cossa <azafoalexandre@outlook.com>
2026-04-21 18:12:35 +0200

    feat: add category management with form handling and dashboard integration

---
+++ resources/views/components/dashboard/⚡category/category.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Livewire\Forms\CategoryForm;
+use App\Models\Category;
+use App\Traits\ShowMessage;
+use Livewire\Component;
+
+new class extends Component
+{
+    use ShowMessage;
+
+    public bool $showCategoryForm = false;
+    
+    public $allCategories = [];
+    public $categories = [];
+
+    public CategoryForm $categoryForm;
+
+    public function mount(){
+        $this->categories = $this->getCategories()->where('parent_id', null)->load('children');
+    }
+
+    public function saveCategory(){
+        if($this->categoryForm->save()){
+            $this->showMessage(type: 'Success', message: 'Categoria salva com sucesso!');
+            $this->showCategoryForm = false;
+            $this->categories = $this->getCategories();
+
+        }else{
+            $this->showMessage(type: 'Error', message: 'Erro ao salvar categoria!');
+        }
+    }
+
+    private function getCategories(){
+        return Category::all();
+    }
+};
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -37,6 +37,12 @@
             </a>
         </li>
         <li  class="p-2.5 rounded-md mt-2.5 hover:bg-white/19">
+            <a href="{{ route('dashboard.categories') }}" class="flex gap-2.5">
+            <x-icons.category fill="#fff"/>
+            Categorias
+            </a>
+        </li>
+        <li  class="p-2.5 rounded-md mt-2.5 hover:bg-white/19">
             <a href="{{ route('dashboard.suppliers') }}" class="flex gap-2.5">
             <span class="icon">
                 <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
