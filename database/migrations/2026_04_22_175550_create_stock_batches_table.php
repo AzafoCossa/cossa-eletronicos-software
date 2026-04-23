e8453105916822ebf49e8f3bbf4f04123993cf5d
Azafo Cossa <azafoalexandre@outlook.com>
2026-04-23 16:18:56 +0200

    Add StockBatch model and migration for managing stock batches

---
+++ database/migrations/2026_04_22_175550_create_stock_batches_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\ProductVariant;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_batches', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('total_quantity');
+            $table->unsignedInteger('quantity_remaining');
+            $table->unsignedBigInteger('cost_price');
+            $table->foreignIdFor(ProductVariant::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_batches');
+    }
+};
