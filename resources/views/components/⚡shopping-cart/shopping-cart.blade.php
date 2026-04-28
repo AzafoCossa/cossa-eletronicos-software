44e8beae4b3e31784561d3a261afe9229e11d325
Azafo Cossa <azafoalexandre@outlook.com>
2026-04-28 18:35:21 +0200

    Implement item management in shopping cart: add methods for adding, removing, and deleting items with success/error messages.

--- resources/views/components/⚡shopping-cart/shopping-cart.blade.php
+++ resources/views/components/⚡shopping-cart/shopping-cart.blade.php
@@ -17,13 +17,13 @@
                     </div>
                   </div>
                   <div class="bg-cream/56 rounded-xl flex gap-3 text-base text-black">
-                    <button class="p-2 flex justify-center items-center bg-gray-300 rounded-xl cursor-pointer">
+                    <button type="button" wire:click="removeItem({{$item['id']}})" class="p-2 flex justify-center items-center bg-gray-300 rounded-xl cursor-pointer">
                       <svg width="10" height="2" viewBox="0 0 10 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10 1C10 1.55313 9.65625 2 9.23077 2H0.769231C0.34375 2 0 1.55313 0 1C0 0.446875 0.34375 0 0.769231 0H9.23077C9.65625 0 10 0.446875 10 1Z" fill="black" fill-opacity="0.64"/>
                       </svg>
                     </button>
                     <p>{{ $item['quantity'] }}</p>
-                    <button class="p-2 flex justify-center items-center bg-gray-300 rounded-xl cursor-pointer">
+                    <button type="button" wire:click="addItem({{$item['id']}})" class="p-2 flex justify-center items-center bg-gray-300 rounded-xl cursor-pointer">
                       <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M5.76923 0.769231C5.76923 0.34375 5.42548 0 5 0C4.57452 0 4.23077 0.34375 4.23077 0.769231V4.23077H0.769231C0.34375 4.23077 0 4.57452 0 5C0 5.42548 0.34375 5.76923 0.769231 5.76923H4.23077V9.23077C4.23077 9.65625 4.57452 10 5 10C5.42548 10 5.76923 9.65625 5.76923 9.23077V5.76923H9.23077C9.65625 5.76923 10 5.42548 10 5C10 4.57452 9.65625 4.23077 9.23077 4.23077H5.76923V0.769231Z" fill="black" fill-opacity="0.64"/>
                       </svg>
@@ -31,7 +31,7 @@
                   </div>
                   <p class="font-bold text-base">{{ moneyFromCents($item['total_price']) }}</p>
 
-                  <button class="cursor-pointer">
+                  <button type="button" wire:click="deleteItem({{$item['id']}})" class="cursor-pointer">
                     <svg width="28" height="32" viewBox="0 0 28 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8.45 1.10625L8 2H2C0.89375 2 0 2.89375 0 4C0 5.10625 0.89375 6 2 6H26C27.1063 6 28 5.10625 28 4C28 2.89375 27.1063 2 26 2H20L19.55 1.10625C19.2125 0.425 18.5188 0 17.7625 0H10.2375C9.48125 0 8.7875 0.425 8.45 1.10625ZM26 8H2L3.325 29.1875C3.425 30.7687 4.7375 32 6.31875 32H21.6812C23.2625 32 24.575 30.7687 24.675 29.1875L26 8Z" fill="black" fill-opacity="0.69"/>
                     </svg>
--- resources/views/components/⚡shopping-cart/shopping-cart.php
+++ resources/views/components/⚡shopping-cart/shopping-cart.php
@@ -1,15 +1,58 @@
 <?php
 
+use App\Models\CartItem;
 use App\Services\CartService;
+use App\Traits\ShowMessage;
+use Illuminate\Support\Arr;
 use Livewire\Component;
 
 new class extends Component
 {
+    use ShowMessage;
+
     private CartService $cartService;
     public $cart;
 
     public function mount()
     {
+        $this->cart = $this->cartService->getCart();
+    }
+
+    public function addItem(CartItem $item){
+        try{
+            $this->cartService->addItem($item->product_variant_id);
+            $this->showMessage(type:'success', message:'Item atualizado com sucesso');
+            $this->cart = $this->cartService->getCart();
+        }catch(\Exception $e){
+            $this->showMessage(type:'error', message:'Nao foi possivel acrescentar a quantidade');
+        }
+    }
+
+    public function removeItem(CartItem $item){
+        $cartItem = Arr::first($this->cart['items'], function($value) use ($item){
+            if($value['id'] === $item->id) return $value;
+        });
+
+        if($cartItem['quantity'] > 1){
+            $qty = --$cartItem['quantity'];
+            $this->cartService->updateItem($cartItem['id'], $qty);
+
+            $this->showMessage(type:'success', message:'O item foi removido');
+
+            return $this->cart = $this->cartService->getCart();
+        }else{
+            $item = CartItem::where('id', $cartItem['id'])->first();
+            $this->deleteItem($item);
+        }
+    }
+
+    public function deleteItem(CartItem $item){
+        $cartItem = Arr::first($this->cart['items'], function($value) use ($item){
+            if($value['id'] === $item->id) return $value;
+        });
+
+        $this->cartService->updateItem($cartItem['id'], -1);
+        $this->showMessage(type:'success', message:'Item removido com sucesso');
         $this->cart = $this->cartService->getCart();
     }
 
