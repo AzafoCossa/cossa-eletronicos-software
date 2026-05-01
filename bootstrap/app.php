a65ccdfc4ea9a13e4ad0204f34968aaaac0b332b
Azafo Cossa <azafoalexandre@outlook.com>
2026-05-01 14:00:08 +0200

    Refactor middleware trustProxies configuration for improved readability

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
-        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
             Request::HEADER_X_FORWARDED_HOST |
             Request::HEADER_X_FORWARDED_PORT |
             Request::HEADER_X_FORWARDED_PROTO
