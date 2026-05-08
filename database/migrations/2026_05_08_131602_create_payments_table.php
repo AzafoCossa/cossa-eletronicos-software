c79ed055bb3529690ad78b8b9ff9c3dd6f9cc892
Azafo Cossa <azafoalexandre@outlook.com>
2026-05-08 16:04:42 +0200

    Created Payment model and corresponding migration

---
+++ database/migrations/2026_05_08_131602_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Order;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 10);
+            $table->unsignedBigInteger('paid_amount')->default(0);
+            $table->string('payment_status')->default('UNCONFIRMED');
+            $table->string('payment_proof')->nullable();
+            $table->foreignIdFor(Order::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
